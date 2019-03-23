cb19149a30806c6d9854fe27f997d8cd4c1aacec
Julian Garcia <jg.garcia@globant.com>
2019-03-23 10:51:32 -0600

    main.py, merge_sort function added

--- main.py
+++ main.py
@@ -27,6 +27,42 @@ def buble_sort(array):
         if count == 0:
             break
     return array
+def order_two_arrays(res, l_arr, r_arr):
+    i, j, k= (0, 0, 0)
+    while i < len(l_arr) and j < len(r_arr):
+        if l_arr[i] >= r_arr[j]:
+            res[k] = r_arr[j]   
+            j += 1
+        else:
+            res[k] = l_arr[i]
+            i += 1
+        k += 1
+    while i < len(l_arr):
+        res[k] = l_arr[i]
+        i += 1
+        k += 1
+
+    while j < len(r_arr):
+        res[k] = r_arr[j]
+        j += 1
+        k += 1
+
+    return res
+
+def merge_sort(arr):
+    l = len(arr)
+    l_arr = arr[:int(l/2)]
+    r_arr = arr[int(l/2):]
+
+    if l > 1:
+        l_res = merge_sort(l_arr)
+        r_res = merge_sort(r_arr)
+        res =  order_two_arrays(arr, l_res, r_res)
+        return res
+    else:
+        return arr
+        
+
 
 
 def main(param):
